3a794156c19038e9ff477cc27e8515c1829f0d54
Adames Hodelin <9205134+adames@users.noreply.github.com>
2025-03-10 04:15:56 +0000

    restarted shell for science, adding new host

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,10 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   server: {
-    allowedHosts: ["vt7shf-5173.csb.app", "vt7shf-5174.csb.app"],
+    allowedHosts: [
+      "vt7shf-5173.csb.app",
+      "vt7shf-5174.csb.app",
+      "m8vcmd-5173.csb.app",
+    ],
   },
 });
